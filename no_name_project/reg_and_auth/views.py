9bf1d8e1208aa34268b66cd9629c8161d9c74c59
kotosusl <kotosusl@gmail.com>
2023-11-04 03:16:00 +0700

    validate

--- no_name_project/reg_and_auth/views.py
+++ no_name_project/reg_and_auth/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import RegisterAndLoginForm, NewProfileForm
 
 
 def register(request):
     if request.method == 'POST':
-        email = request.POST.get('email', 'Undefined')
-        password = request.POST.get('password', 'Undefined')
-        return HttpResponse(f"Форма успешно отправлена {email} {password}")
+        user_form = RegisterAndLoginForm(request.POST)
+        if user_form.is_valid():
+            email = user_form.cleaned_data['email']
+            password = user_form.cleaned_data['password']
+            return HttpResponseRedirect('/reg_and_auth/new_profile')
+        else:
+            return render(request, 'register.html', {'form': user_form})
     else:
         register_form = RegisterAndLoginForm()
         return render(request, 'register.html', {'form': register_form})
@@ -21,9 +25,13 @@ def register(request):
 
 def log_in(request):
     if request.method == 'POST':
-        email = request.POST.get('email', 'Undefined')
-        password = request.POST.get('password', 'Undefined')
-        return HttpResponse(f'Вы успешно вошли {email} {password}')
+        user_form = RegisterAndLoginForm(request.POST)
+        if user_form.is_valid():
+            email = user_form.cleaned_data['email']
+            password = user_form.cleaned_data['password']
+            return HttpResponseRedirect('/main_app/main')
+        else:
+            return render(request, 'log_in.html', {'form': user_form})
     else:
         login_form = RegisterAndLoginForm()
         return render(request, 'log_in.html', {'form': login_form})
@@ -37,13 +45,18 @@ def log_in(request):
 
 def new_profile(request):
     if request.method == "POST":
-        name = request.POST.get('name', 'Undefined')
-        surname = request.POST.get('surname', 'Undefined')
-        father = request.POST.get('father', 'Undefined')
-        gender = request.POST.get('gender', 'Undefined')
-        ages = request.POST.get('ages', 'Undefined')
-        message_places = request.POST.getlist('message_places', [])
-        return HttpResponse(f'Ваш профиль успешно сохранён {name} {surname} {father} {gender} {ages} {message_places}')
+        user_form = NewProfileForm(request.POST)
+        if user_form.is_valid():
+            name = user_form.cleaned_data['name']
+            surname = user_form.cleaned_data['surname']
+            father = user_form.cleaned_data['father']
+            gender = user_form.cleaned_data['gender']
+            ages = user_form.cleaned_data['ages']
+            message_places = request.POST.getlist('message_places', [])
+            # return HttpResponseRedirect('/main_app/main')
+            return HttpResponse(f'Ваш профиль успешно сохранён {name} {surname} {father} {gender} {ages} {message_places}')
+        else:
+            return render(request, 'new_profile.html', {'form': user_form})
     else:
         new_profile_form = NewProfileForm()
         return render(request, 'new_profile.html', {'form': new_profile_form})
